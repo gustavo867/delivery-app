c881f9c2261621476432c0bbbdec7da4d6cbebd5
gustavo867 <tartatavo3@gmail.com>
2020-08-11 16:24:07 -0300

    :wrench: build

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, FlatList, Image, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, FlatList, Image, StyleSheet, } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 const Profile = () => {
   return (
     <View style={styles.container}>
+        <LinearGradient
+          colors={['#F6F5F5' , 'rgba(255, 255, 255, 0.2)']}
+          style={{ position: 'absolute', height: '100%' , left: 0, right: 0, top: 0,}}
+        />
       <Text>Hello</Text>
     </View>
   )
@@ -13,7 +18,5 @@ export default Profile
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
 })
